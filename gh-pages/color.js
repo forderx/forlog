de360b207bf6ead469a7226c03f2cd8b91824af6
forderx <63668848+forderx@users.noreply.github.com>
2020-04-15 13:01:10 +0500

    Update color.js

--- gh-pages/color.js
+++ gh-pages/color.js
@@ -1,1 +1,10 @@
+var elem = document.getElementById("othercolor");
+      othercolor.onclick = function () {
+        if(this.style.backgroundColor)
+          this.style.backgroundColor = "white";
+        if(this.style.color)
+          this.style.color = "black";
+        else
+           this.style.backgroundColor = "#0e0e10";
+      }
 
