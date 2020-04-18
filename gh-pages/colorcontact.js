47cdf9207df0a18761c31ea6a84ab9d11d28f9c4
forderx <63668848+forderx@users.noreply.github.com>
2020-04-18 18:58:41 +0500

    Update colorcontact.js

--- gh-pages/colorcontact.js
+++ gh-pages/colorcontact.js
@@ -3,7 +3,6 @@ function changeothercolor(){
             chbox=document.getElementById('bone');
             if(chbox.checked){
                 document.getElementsByClassName("mainmenu")[0].style.background = "Gainsboro";
-                document.getElementsByClassName("mainblock")[0].style.background = "Gainsboro";
                 document.body.style.background = "white";
                 document.getElementsByTagName("a")[0].style.color = "black";
                 document.getElementsByTagName("a")[1].style.color = "black";
@@ -11,6 +10,8 @@ function changeothercolor(){
                 document.getElementsByClassName("topmain")[0].style.color = "white";
                 document.getElementsByClassName("topmain")[0].style.background = "black";
                 document.getElementsByClassName("toptwo")[0].style.color = "black";
+                document.getElementById("mainmenu").style.boxShadow = "0 0 0px 0px";
+                document.getElementsByTagName("i").style.color = "black";
             }else {
                 document.getElementsByClassName("mainmenu")[0].style.background = "#0D0D0C";
                 document.getElementsByClassName("mainblock")[0].style.background = "#0D0D0C";
@@ -21,5 +22,7 @@ function changeothercolor(){
                 document.getElementsByClassName("topmain")[0].style.color = "white";
                 document.getElementsByClassName("topmain")[0].style.background = "black";
                 document.getElementsByClassName("toptwo")[0].style.color = "black";
+                document.getElementById("mainblock").style.boxShadow = "0 0 5px 2px";
+                document.getElementsByTagName("i").style.color = "white";
             }
         }
