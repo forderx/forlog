193591348cd0ec59bf9657874995c1dcfbf922c5
forderx <63668848+forderx@users.noreply.github.com>
2020-04-18 18:46:53 +0500

    Update colorcontact.js

--- gh-pages/colorcontact.js
+++ gh-pages/colorcontact.js
@@ -2,7 +2,7 @@ function changeothercolor(){
             let chbox;
             chbox=document.getElementById('bone');
             if(chbox.checked){
-                document.getElementById("mainmenu").style.background = "Gainsboro";
+                document.getElementByClassName("mainmenu")[0].style.background = "Gainsboro";
                 document.getElementById("mainblock").style.background = "Gainsboro";
                 document.body.style.background = "white";
                 document.getElementsByTagName("a")[0].style.color = "black";
@@ -12,7 +12,7 @@ function changeothercolor(){
                 document.getElementsByClassName("topmain")[0].style.background = "black";
                 document.getElementsByClassName("toptwo")[0].style.color = "black";
             }else {
-            document.getElementById("mainmenu").style.background = "#0D0D0C";
+                document.getElementByClassName("mainmenu")[0].style.background = "#0D0D0C";
                 document.getElementById("mainblock").style.background = "#0D0D0C";
                 document.body.style.background = "#0e0e10";
                 document.getElementsByTagName("a")[0].style.color = "black";
