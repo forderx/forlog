d992679b0c98e9dc3ccfd487afdfe45b2802b090
forderx <63668848+forderx@users.noreply.github.com>
2020-04-18 19:05:40 +0500

    Update colorcontact.js

--- gh-pages/colorcontact.js
+++ gh-pages/colorcontact.js
@@ -16,7 +16,6 @@ function changeothercolor(){
                 document.getElementsByTagName("i")[2].style.color = "black";
             }else {
                 document.getElementsByClassName("mainmenu")[0].style.background = "#0D0D0C";
-                document.getElementsByClassName("mainblock")[0].style.background = "#0D0D0C";
                 document.body.style.background = "#0e0e10";
                 document.getElementsByTagName("a")[0].style.color = "white";
                 document.getElementsByTagName("a")[1].style.color = "white";
